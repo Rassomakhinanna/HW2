bcdda94684a3e1c27b1cb4f9c8d1a6a9c743254c
annarassomahina <pupkinppppp@gmail.com>
2022-09-11 20:44:12 +0400

    HW2

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Hogwarts {
-}
+public class Hogwarts {
+    String name;
+        int magic;
+        int trans;
+
+
+    public void compareStudents(Hogwarts student2){
+            int power1 = this.magic + this.trans;
+            int power2 = student2.magic + student2.trans;
+            if (power1>power2) {
+                System.out.println(this.name + " сильнее, чем " + student2.name);
+            } else if (power2>power1) {
+                System.out.println(student2.name + " сильнее, чем " + this.name);
+            } else if (power1==power2) {
+                System.out.println("Силы " + this.name + "и" + student2 + "равны");
+            }
+        }
+
+
+
+
+    public int getTrans() {
+            return trans;
+        }
+
+        public void setTrans(int trans) {
+            if (trans<0 || trans > 100) {
+                throw new RuntimeException("Ошибка");
+            }
+            this.trans = trans;
+        }
+
+        public int getMagic() {
+            return magic;
+        }
+
+        public void setMagic(int magic) {
+            if (magic<0 || magic > 100) {
+                throw new RuntimeException("Ошибка");
+            }
+            this.magic = magic;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gryffindor RY = new Gryffindor("Рон Уизли", 6, 16, 26, 36, 46);
+        Gryffindor GG = new Gryffindor("Гермиона Грейнджер", 2, 12, 22, 32, 42);
+        Ravenclaw PP = new Ravenclaw("Падма Патил", 4,14,24,34,44,54);
+        Ravenclaw MB = new Ravenclaw("Маркус Белби", 9,19,29,39,49,59);
+        Hufflepuff ZS = new Hufflepuff("Захария Смит", 0,1,2,3,4);
+        Hufflepuff SD = new Hufflepuff("Седрик Диггори", 2,3,4,5,6);
+        Slytherin GrG = new Slytherin("Грегори Гойл", 3,4,5,6,7,8, 6);
+        Slytherin DM = new Slytherin("Драко Малфой", 6,7,8,9,10,11,23);
+        System.out.println(RY);
+        System.out.println(GG);
+        System.out.println(PP);
+        System.out.println(MB);
+        System.out.println(ZS);
+        System.out.println(SD);
+        System.out.println(GrG);
+        System.out.println(DM);
+
+        RY.compareStudents(GG);
+        PP.compareStudents(MB);
+        ZS.compareStudents(SD);
+        GrG.compareStudents(DM);
+
+        GG.compareStudents(ZS);
+
     }
 }
